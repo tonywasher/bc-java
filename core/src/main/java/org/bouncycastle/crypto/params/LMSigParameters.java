38288ddb6986ca1962f4db45236f9834162dc21a
Peter Dettman <peter.dettman@bouncycastle.org>
2026-09-18 15:36:27 +0700

    LMS: LMOtsParameters/LMSigParameters get static create factories and typecode equals/hashCode with the protected constructors deprecated; LM-OTS p/ls/sigLen are derived per RFC 8554 Appendix B, correcting the n=24 sigLen values, with a table test

--- core/src/main/java/org/bouncycastle/crypto/params/LMSigParameters.java
+++ core/src/main/java/org/bouncycastle/crypto/params/LMSigParameters.java
@@ -8,29 +8,29 @@ import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
 
 public class LMSigParameters
 {
-    public static final LMSigParameters lms_sha256_n32_h5 = new LMSigParameters(0x05, 32, 5, NISTObjectIdentifiers.id_sha256);
-    public static final LMSigParameters lms_sha256_n32_h10 = new LMSigParameters(0x06, 32, 10, NISTObjectIdentifiers.id_sha256);
-    public static final LMSigParameters lms_sha256_n32_h15 = new LMSigParameters(0x07, 32, 15, NISTObjectIdentifiers.id_sha256);
-    public static final LMSigParameters lms_sha256_n32_h20 = new LMSigParameters(0x08, 32, 20, NISTObjectIdentifiers.id_sha256);
-    public static final LMSigParameters lms_sha256_n32_h25 = new LMSigParameters(0x09, 32, 25, NISTObjectIdentifiers.id_sha256);
+    public static final LMSigParameters lms_sha256_n32_h5 = create(0x05, 32, 5, NISTObjectIdentifiers.id_sha256);
+    public static final LMSigParameters lms_sha256_n32_h10 = create(0x06, 32, 10, NISTObjectIdentifiers.id_sha256);
+    public static final LMSigParameters lms_sha256_n32_h15 = create(0x07, 32, 15, NISTObjectIdentifiers.id_sha256);
+    public static final LMSigParameters lms_sha256_n32_h20 = create(0x08, 32, 20, NISTObjectIdentifiers.id_sha256);
+    public static final LMSigParameters lms_sha256_n32_h25 = create(0x09, 32, 25, NISTObjectIdentifiers.id_sha256);
 
-    public static final LMSigParameters lms_sha256_n24_h5 = new LMSigParameters(0x0a, 24, 5, NISTObjectIdentifiers.id_sha256);
-    public static final LMSigParameters lms_sha256_n24_h10 = new LMSigParameters(0x0b, 24, 10, NISTObjectIdentifiers.id_sha256);
-    public static final LMSigParameters lms_sha256_n24_h15 = new LMSigParameters(0x0c, 24, 15, NISTObjectIdentifiers.id_sha256);
-    public static final LMSigParameters lms_sha256_n24_h20 = new LMSigParameters(0x0d, 24, 20, NISTObjectIdentifiers.id_sha256);
-    public static final LMSigParameters lms_sha256_n24_h25 = new LMSigParameters(0x0e, 24, 25, NISTObjectIdentifiers.id_sha256);
+    public static final LMSigParameters lms_sha256_n24_h5 = create(0x0a, 24, 5, NISTObjectIdentifiers.id_sha256);
+    public static final LMSigParameters lms_sha256_n24_h10 = create(0x0b, 24, 10, NISTObjectIdentifiers.id_sha256);
+    public static final LMSigParameters lms_sha256_n24_h15 = create(0x0c, 24, 15, NISTObjectIdentifiers.id_sha256);
+    public static final LMSigParameters lms_sha256_n24_h20 = create(0x0d, 24, 20, NISTObjectIdentifiers.id_sha256);
+    public static final LMSigParameters lms_sha256_n24_h25 = create(0x0e, 24, 25, NISTObjectIdentifiers.id_sha256);
 
-    public static final LMSigParameters lms_shake256_n32_h5 = new LMSigParameters(0x0f, 32, 5, NISTObjectIdentifiers.id_shake256_len);
-    public static final LMSigParameters lms_shake256_n32_h10 = new LMSigParameters(0x10, 32, 10, NISTObjectIdentifiers.id_shake256_len);
-    public static final LMSigParameters lms_shake256_n32_h15 = new LMSigParameters(0x11, 32, 15, NISTObjectIdentifiers.id_shake256_len);
-    public static final LMSigParameters lms_shake256_n32_h20 = new LMSigParameters(0x12, 32, 20, NISTObjectIdentifiers.id_shake256_len);
-    public static final LMSigParameters lms_shake256_n32_h25 = new LMSigParameters(0x13, 32, 25, NISTObjectIdentifiers.id_shake256_len);
+    public static final LMSigParameters lms_shake256_n32_h5 = create(0x0f, 32, 5, NISTObjectIdentifiers.id_shake256_len);
+    public static final LMSigParameters lms_shake256_n32_h10 = create(0x10, 32, 10, NISTObjectIdentifiers.id_shake256_len);
+    public static final LMSigParameters lms_shake256_n32_h15 = create(0x11, 32, 15, NISTObjectIdentifiers.id_shake256_len);
+    public static final LMSigParameters lms_shake256_n32_h20 = create(0x12, 32, 20, NISTObjectIdentifiers.id_shake256_len);
+    public static final LMSigParameters lms_shake256_n32_h25 = create(0x13, 32, 25, NISTObjectIdentifiers.id_shake256_len);
 
-    public static final LMSigParameters lms_shake256_n24_h5 = new LMSigParameters(0x14, 24, 5, NISTObjectIdentifiers.id_shake256_len);
-    public static final LMSigParameters lms_shake256_n24_h10 = new LMSigParameters(0x15, 24, 10, NISTObjectIdentifiers.id_shake256_len);
-    public static final LMSigParameters lms_shake256_n24_h15 = new LMSigParameters(0x16, 24, 15, NISTObjectIdentifiers.id_shake256_len);
-    public static final LMSigParameters lms_shake256_n24_h20 = new LMSigParameters(0x17, 24, 20, NISTObjectIdentifiers.id_shake256_len);
-    public static final LMSigParameters lms_shake256_n24_h25 = new LMSigParameters(0x18, 24, 25, NISTObjectIdentifiers.id_shake256_len);
+    public static final LMSigParameters lms_shake256_n24_h5 = create(0x14, 24, 5, NISTObjectIdentifiers.id_shake256_len);
+    public static final LMSigParameters lms_shake256_n24_h10 = create(0x15, 24, 10, NISTObjectIdentifiers.id_shake256_len);
+    public static final LMSigParameters lms_shake256_n24_h15 = create(0x16, 24, 15, NISTObjectIdentifiers.id_shake256_len);
+    public static final LMSigParameters lms_shake256_n24_h20 = create(0x17, 24, 20, NISTObjectIdentifiers.id_shake256_len);
+    public static final LMSigParameters lms_shake256_n24_h25 = create(0x18, 24, 25, NISTObjectIdentifiers.id_shake256_len);
 
     private static Map<Object, LMSigParameters> paramBuilders = new HashMap<Object, LMSigParameters>()
     {
@@ -61,17 +61,54 @@ public class LMSigParameters
         }
     };
 
+    private static LMSigParameters create(int type, int m, int h, ASN1ObjectIdentifier digestOid)
+    {
+        return new LMSigParameters(type, m, h, digestOid);
+    }
+
     private final int type;
     private final int m;
     private final int h;
     private final ASN1ObjectIdentifier digestOid;
 
+    /**
+     * @deprecated The parameter sets are the static fields of this class, looked up by
+     * {@link #getParametersForType(int)}; this class is not intended to be subclassed and this constructor will be
+     * made private.
+     */
+    @Deprecated
     protected LMSigParameters(int type, int m, int h, ASN1ObjectIdentifier digestOid)
     {
         this.type = type;
         this.m = m;
         this.h = h;
         this.digestOid = digestOid;
+    }
+
+    /**
+     * The typecode identifies the parameter set: the rest of the values are derived from it.
+     * <p>
+     * The instances are interned - every lookup hands back one of the static fields - so this agrees with the
+     * reference equality it replaces. It states the intent instead of leaving callers to rely on the interning.
+     */
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (!(obj instanceof LMSigParameters))
+        {
+            return false;
+        }
+
+        LMSigParameters that = (LMSigParameters)obj;
+        return type == that.type;
+    }
+
+    public int hashCode()
+    {
+        return type;
     }
 
     public int getType()
